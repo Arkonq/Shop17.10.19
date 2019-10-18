1b996ff7eb015ed95e474f0c4a2e591cba8e8de2
arkonq@gmail.com <arkonq@gmail.com>
2019-10-18 21:38:12 +0600

    Работа за 18.10

--- Shop.UI/Program.cs
+++ Shop.UI/Program.cs
@@ -2,6 +2,7 @@
 using Shop.DataAccess;
 using Shop.Domain;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -33,9 +34,36 @@ namespace Shop.UI
 
 		static void Main(string[] args)
 		{
+			Search();
+			//Pagination();
 			//Test();
 			//Registration();
-			SignIn();
+			//SignIn();
+		}
+
+		static void ProcessCollections()
+		{
+			List<string> cityNames = new List<string>
+			{
+				"Almaty", "Ankara", "Boriswill", "Nur-Sultan", "Yalta"
+			};
+
+			List<string> processedCityNames = new List<string>(); // для поиска товаров от пользователя
+			foreach (string name in cityNames)
+			{
+				if (name.Contains("-"))
+				{
+					processedCityNames.Add(name);
+				}
+			}
+
+			var result = from name
+									 in cityNames
+									 where name.Contains("-")
+									 select name;
+
+			var shortResult = cityNames.Where(name => name.Contains("-"));
+			var shortResult2 = cityNames.Select(name => name.Contains("-"));
 		}
 
 		private static void Test()
@@ -43,15 +71,15 @@ namespace Shop.UI
 			Category category = new Category
 			{
 				Name = "Бытовая техника",
-				ImagePath = "C:/data",
+				//ImagePath = "C:/data",
 			};
 
 			Item item = new Item
 			{
 				Name = "Пылесос",
-				ImagePath = "C:/data/items",
-				Price = 25999,
-				Description = "Обычный пылесос",
+				//ImagePath = "C:/data/items",
+				//Price = 25999,
+				//Description = "Обычный пылесос",
 				CategoryId = category.Id
 			};
 
@@ -70,8 +98,26 @@ namespace Shop.UI
 				context.Users.Add(user);
 				context.Items.Add(item);
 				context.Categories.Add(category);
+
+				var result = context.Categories.ToList();
+				//context.Remove(category);
+
+				//var quariedCategories = context.Categories.Where(x => x.CreationDate.Date < new System.DateTime(2017,10,5).Date);
+
+				//var funnyResult = quariedCategories.Select(x => new
+				//{
+				//	Id = x.Id,
+				//	StartDate = x.CreationDate,
+				//	FunnyName = "Funny" + x.Name
+				//});
+
+				//var finalResult = funnyResult.ToList();
+
 				context.SaveChanges();
 			}
+
+			string data = "12345sdf";
+			var newString = data.ExtractOnlyText();
 		}
 
 
@@ -121,6 +167,99 @@ namespace Shop.UI
 				if (user.Count() == 1)
 				{
 					Console.WriteLine("Введите секретный код: ");
+				}
+			}
+		}
+		static void Search()
+		{
+			int page = 1, pageSize = 3, pages = 1, items = 0;
+			string search;
+			List<Item> result;
+			IQueryable<Item> query;
+			Console.WriteLine("Введите искомый товар/категорию");
+			search = Console.ReadLine();
+			using (var context = new ShopContext(connectionString))
+			{
+				query = from item in context.Items
+								where item.Name.Contains(search)
+								select item;
+				items = query.Count();
+			}
+			if(items % pageSize == 0)
+			{
+				pages = items / pageSize;
+			}
+			else
+			{
+				pages = items / pageSize + 1;
+			}
+			ShopPage(page, pageSize, pages, search, out result, out query);
+
+			while (page != 0)
+			{
+				Console.WriteLine("Введите страницу (0 для выхода):");
+				if (!Int32.TryParse(Console.ReadLine(), out page))
+				{
+					Console.WriteLine("Введенная страница не является числом.");
+					Console.ReadLine();
+					Console.Clear();
+					continue;
+				}
+				Console.Clear();
+			}
+		}
+
+		private static void ShopPage(int page, int pageSize,int pages, string search, out List<Item> result, out IQueryable<Item> query)
+		{
+			if (page < 0)
+			{
+				page = -page;
+			}
+			using (var context = new ShopContext(connectionString))
+			{
+				query = from item in context.Items
+								where item.Name.Contains(search)
+								select item;
+				var paging = query.Skip((page - 1) * pageSize).Take(pageSize);
+				result = paging.ToList();
+			}
+			Console.WriteLine($"Page {page}:");
+			foreach (var item in result)
+			{
+				Console.WriteLine($"\t{item.Name} - {item.Price} тг");
+			}
+		}
+
+		static void Pagination()
+		{
+			int page = 0, pageSize = 3;
+			List<Item> result;
+			while (page != 0)
+			{
+				Console.WriteLine("Введите страницу (0 для выхода):");
+
+				if (!Int32.TryParse(Console.ReadLine(), out page))
+				{
+					Console.WriteLine("Введенная страница не является числом.");
+					Console.ReadLine();
+					Console.Clear();
+					continue;
+				}
+				Console.Clear();
+				using (var context = new ShopContext(connectionString))
+				{
+					var query = from item in context.Items
+											orderby item.Name
+											select item;
+
+					var paging = query.Skip((page - 1) * pageSize).Take(pageSize);
+
+					result = paging.ToList();
+				}
+				Console.WriteLine($"Page {page}:");
+				foreach (var item in result)
+				{
+					Console.WriteLine($"\t{item.Name} - {item.Price} тг");
 				}
 			}
 		}
